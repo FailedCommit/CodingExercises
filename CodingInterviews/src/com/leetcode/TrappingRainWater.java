6ec41877a675c3c509bf11679adb5ad9e9a103b9
Mohit Chilkoti <27889519+CodeCramp@users.noreply.github.com>
2018-08-28 12:04:26 +0530

    TwoPointer & Stack : Trapping Rain Water.
    # Updates problem description.

--- CodingInterviews/src/com/leetcode/TrappingRainWater.java
+++ CodingInterviews/src/com/leetcode/TrappingRainWater.java
@@ -3,6 +3,9 @@ package com.leetcode;
 import java.util.Stack;
 
 /**
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it is able to trap after raining.
+ * 
  * https://leetcode.com/problems/trapping-rain-water/description/
  */
 public class TrappingRainWater {
