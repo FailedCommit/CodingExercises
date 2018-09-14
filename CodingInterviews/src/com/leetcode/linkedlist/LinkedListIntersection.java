3e7f8f4162a1b2a3075c390f6c40af1b35795bb1
Mohit Chilkoti <27889519+CodeCramp@users.noreply.github.com>
2018-09-15 03:37:52 +0530

    Link-List: Improves finding intersection of two linked-lists.

--- CodingInterviews/src/com/leetcode/linkedlist/LinkedListIntersection.java
+++ CodingInterviews/src/com/leetcode/linkedlist/LinkedListIntersection.java
@@ -4,33 +4,37 @@ public class LinkedListIntersection {
 
   public static void main(String[] args) {
     LinkedListIntersection lli = new LinkedListIntersection();
-    
+
     ListNode intersection = new ListNode(40, new ListNode(50, new ListNode(60, null)));
     ListNode headA = new ListNode(10, new ListNode(20, new ListNode(30, intersection)));
     ListNode headB = new ListNode(111, intersection);
     System.out.println("Given lists converge at Node : " + lli.getIntersectionNode(headA, headB)); // 40
-    
+    System.out.println("Given lists converge at Node : " + lli.getIntersectionNodeImproved(headA, headB)); // 40
+
     ListNode intersectionXY = new ListNode(1, null);
     ListNode headX = intersectionXY;
     ListNode headY = intersectionXY;
     System.out.println("Given lists converge at Node : " + lli.getIntersectionNode(headX, headY)); // 1
-    
+    System.out.println("Given lists converge at Node : " + lli.getIntersectionNodeImproved(headX, headY)); // 1
+
     ListNode intersectionMN = new ListNode(45, new ListNode(50, new ListNode(60, null)));
     ListNode headM = intersectionMN;
     ListNode headN = new ListNode(10, new ListNode(20, new ListNode(30, intersectionMN)));
     System.out.println("Given lists converge at Node : " + lli.getIntersectionNode(headM, headN)); // 45
-    
+    System.out.println("Given lists converge at Node : " + lli.getIntersectionNodeImproved(headM, headN)); // 45
+
     // No intersection
     ListNode headC = new ListNode(10, new ListNode(20, new ListNode(30, null)));
     ListNode headD = new ListNode(111, null);
     System.out.println("Given lists converge at Node : " + lli.getIntersectionNode(headC, headD)); // null
+    System.out.println("Given lists converge at Node : " + lli.getIntersectionNodeImproved(headC, headD)); // null
   }
 
   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    if(headA == null || headB == null) return null;
-    if(headA == headB) return headA;
-    if(headA.next == headB) return headB;
-    if(headB.next == headA) return headA;
+    if (headA == null || headB == null) return null;
+    if (headA == headB) return headA;
+    if (headA.next == headB) return headB;
+    if (headB.next == headA) return headA;
     int lenA = 0;
     int lenB = 0;
     ListNode tempA = headA;
@@ -63,11 +67,22 @@ public class LinkedListIntersection {
       tempA = tempA.next;
       tempB = tempB.next;
     }
-    if(tempA == null || tempA == tempB) return tempA;
+    if (tempA == null || tempA == tempB) return tempA;
     return tempA.next;
   }
 
-  
+  public ListNode getIntersectionNodeImproved(ListNode headA, ListNode headB) {
+    ListNode tempA = headA;
+    ListNode tempB = headB;
+
+    while (tempA != tempB) {
+      tempA = tempA == null ? headB : tempA.next;
+      tempB = tempB == null ? headA : tempB.next;
+    }
+    return tempB;
+  }
+
+
   // Inner Class
   static class ListNode {
     int val;
