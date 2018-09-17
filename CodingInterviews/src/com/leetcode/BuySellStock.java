f6c721634d148e30799cdd113d3d7c20f3a9ddaf
Mohit Chilkoti <27889519+CodeCramp@users.noreply.github.com>
2018-09-18 03:06:26 +0530

    Arrays: Improves -Buy Sell Stocks at maximum profit

--- CodingInterviews/src/com/leetcode/BuySellStock.java
+++ CodingInterviews/src/com/leetcode/BuySellStock.java
@@ -12,23 +12,14 @@ public class BuySellStock {
   }
   
   public int maxProfit_question121(int[] prices) {
-    int buy = 0;
-    int sell = 0;
-    int min = 0;
-    int max = 0;
-    int profit = 0;
-    for(int i = 1; i < prices.length; i++) {
-      if(prices[i] > prices[max]) max = i;
-      if(prices[i] < prices[min]) {
-        min = i;
-        max = i;
-      }
-      if(prices[max] - prices[min] > profit) {
-        buy = min;
-        sell = max;
-        profit = prices[sell] - prices[buy];
-      }
+    int minprice = Integer.MAX_VALUE;
+    int maxprofit = 0;
+    for (int i = 0; i < prices.length; i++) {
+        if (prices[i] < minprice)
+            minprice = prices[i];
+        else if (prices[i] - minprice > maxprofit)
+            maxprofit = prices[i] - minprice;
     }
-    return profit;
+    return maxprofit;
   }
 }
