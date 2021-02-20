df8dc1531e678904cc0c359bf585403bb2e27dae
Mohit Kumar <mkumar@tekion.com>
2021-02-20 11:22:16 +0530

    Implements Sliding Window coding pattern

---
+++ src/com/tools/slidingwindow/MaxSumSubarray.java
@@ -0,0 +1,25 @@
+package com.tools.slidingwindow;
+
+public class MaxSumSubarray {
+    public static void main(String[] args) {
+        int[] input = new int[] {2, 1, 5, 1, 3, 2};
+        System.out.println(maxSumSubarray(input, 3));
+
+        input = new int[] {2, 3, 4, 1, 5};
+        System.out.println(maxSumSubarray(input, 2));
+    }
+
+    private static int maxSumSubarray(int[] input, int k) {
+        int max = 0;
+        int sum = 0;
+        int winStart = 0;
+        for(int winEnd = 0; winEnd < input.length; winEnd++) {
+            sum += input[winEnd];
+            if(winEnd >= (k - 1)) {
+                max = max < sum ? sum : max;
+                sum -= input[winStart++];
+            }
+        }
+        return max;
+    }
+}
---
+++ src/com/tools/slidingwindow/SubArrayAverage.java
@@ -0,0 +1,25 @@
+package com.tools.slidingwindow;
+
+import java.util.Arrays;
+
+public class SubArrayAverage {
+    public static void main(String[] args) {
+        int[] input = new int[] {1, 3, 2, 6, -1, 4, 1, 8, 2};
+        double[] averages = average(input, 5);
+        System.out.println(Arrays.toString(averages));
+    }
+
+    private static double[] average(int[] input, int windowSize) {
+        double[] result = new double[input.length - windowSize + 1];
+        double windowSum = 0;
+        int windowStart = 0;
+        for(int windowEnd = 0; windowEnd < input.length; windowEnd++) {
+            windowSum += input[windowEnd];
+            if(windowEnd >= windowSize - 1) {
+                result[windowStart] = windowSum / windowSize;
+                windowSum -= input[windowStart++];
+            }
+        }
+        return result;
+    }
+}
